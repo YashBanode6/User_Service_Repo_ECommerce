5dcf8177e6823794336b3cb298946aa898f11d9c
YashB <banodeyash06@gmail.com>
2026-03-31 01:21:59 +0530

    Working with user response error handling

--- src/main/java/com/example/user_service/pojo/ErrorResponse.java
+++ src/main/java/com/example/user_service/pojo/ErrorResponse.java
@@ -1,9 +1,10 @@
 package com.example.user_service.pojo;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 public class ErrorResponse {
@@ -16,5 +17,26 @@ public class ErrorResponse {
 		this.status = status;
 		this.timestamp = timestamp;
 	}
+	    public void rotate(int[] nums, int k) {
+	        int n = nums.length;
+
+	        int d = k % n;
+
+	        List<Integer> list = new ArrayList<>();
+	        for(int i = 0; i < d; i++){
+	            list.add(nums[n-i-1]);
+	        }
+
+
+	        for(int i = n -  -1; i >= 0; i--){
+	           nums[i + d] = nums[i] ;
+	        }
+
+	        int j = 0;
+	        for(int i = d-1; i >= 0; i--){
+	           nums[i] = list.get(j++);
+	        }
+	    }
+	
 
 }
