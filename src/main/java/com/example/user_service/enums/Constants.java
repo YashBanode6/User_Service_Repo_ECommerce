fb92dfed6065a1adb255c503ed26de83b8897aea
YashB <banodeyash06@gmail.com>
2026-04-08 02:07:35 +0530

    adding constants in enum

--- src/main/java/com/example/user_service/enums/Constants.java
+++ src/main/java/com/example/user_service/enums/Constants.java
@@ -1,5 +1,8 @@
 package com.example.user_service.enums;
 
 public enum Constants {
+	USER_ALREADY_EXISTS("User with the given email already exists"),
+	USER_NOT_FOUND("User not found with the given id");
 
+	
 }
